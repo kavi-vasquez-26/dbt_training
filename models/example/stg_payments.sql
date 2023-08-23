57138d31a20a8c055fbcdcb5752762d3b9a39ebb
kavi-vasquez-26 <142338220+kavi-vasquez-26@users.noreply.github.com>
2023-08-23 09:32:24 -0400

    jinja practice

--- models/example/stg_payments.sql
+++ models/example/stg_payments.sql
@@ -2,7 +2,7 @@ select
     id as payment_id
     , orderid as order_id
     , paymentmethod a payment_method
-    , status
+    , status as payment_status
 
     -- amount stored in cents converted to dollars
     , {{cents_to_dollars('amount', 2)}} as amount
